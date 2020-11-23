f97dda422440c105723f585be347c5a2bd39dc23
Jairo Juvenal Escobar Calzada <jairo16_10@hotmail.com>
2020-11-22 19:12:52 -0600

    Added latest test for people list

--- salesloft-challenge-web/src/app/modules/people/pages/people-list/people-list.component.spec.ts
+++ salesloft-challenge-web/src/app/modules/people/pages/people-list/people-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { mockPeopleList } from 'src/app/core/mockups/people-list.mockup';
 import { PeopleService } from 'src/app/core/services/people/people.service';
 
 import { PeopleListComponent } from './people-list.component';
@@ -21,10 +23,38 @@ describe('PeopleListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PeopleListComponent);
     component = fixture.componentInstance;
+    component.elements = mockPeopleList;
+    component.currentPage = 1;
+    component.totalCount = 100;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should bind list of people with details', async() => {
+    expect(component.elements[0].id).toBeTruthy();
+  });
+
+  it('should bind total count of people', async() => {
+    expect(component.totalCount).toBe(100);
+  });
+
+  it('should bind current page', async() => {
+    expect(component.currentPage).toBe(1);
+  });
+
+  it('should display the error message', async() => {
+    fixture = TestBed.createComponent(PeopleListComponent);
+    component = fixture.componentInstance;
+    component.peopleListErrorMsg = "Sample error"
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const errorContainer = fixture.debugElement.query(By.css("#people-list-error")).nativeElement;
+      expect(errorContainer.innerHTML).toBe("Sample error");
+      
+    });
   });
 });
